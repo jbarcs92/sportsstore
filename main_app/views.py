4201daae79478015cda96e54cb852782d51d34f2
jbarcs92 <jbarcl92@gmail.com>
2023-07-13 09:58:52 -0400

    addeded views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-# from .models import Football
+from .models import Football
+from .models import Baseball
+from .models import Basketball
+from .models import Hockey
+from .models import Mma
+from .models import Soccer
 
 def home(request):
     return render(request, 'home.html')
@@ -39,3 +44,39 @@ def hockey_index(request):
     return render(request, 'hockey/index.html', {
         'hockeys': hockeys
     }) 
+
+def football_detail(request, football_id):
+    football = Football.objects.get(id=football_id)
+    return render(request, 'football/detail.html', {
+        'football': football
+    })
+
+def baseball_detail(request, baseball_id):
+    baseball = Baseball.objects.get(id=baseball_id)
+    return render(request, 'baseball/detail.html', {
+        'baseball': baseball
+    })
+
+def basketball_detail(request, basketball_id):
+    basketball = Basketball.objects.get(id=basketball_id)
+    return render(request, 'basketball/detail.html', {
+        'basketball': basketball
+    })
+
+def hockey_detail(request, hockey_id):
+    hockey = Hockey.objects.get(id=hockey_id)
+    return render(request, 'hockey/detail.html', {
+        'hockey': hockey
+    })
+
+def mma_detail(request, mma_id):
+    mma = Mma.objects.get(id=mma_id)
+    return render(request, 'mma/detail.html', {
+        'mma': mma
+    })
+
+def soccer_detail(request, mma_id):
+    mma = Soccer.objects.get(id=soccer_id)
+    return render(request, 'soccer/detail.html', {
+        'soccer': soccer
+    })
